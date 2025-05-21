a2ecd94ccca8b2870e3664f23466a8c2ad3a8272
iori_n <iori.tanabe@gmail.com>
2025-05-21 12:31:36 +0900

    Add select_column function.

--- project.py
+++ project.py
@@ -47,30 +47,104 @@ def write_csv_file(csv_table, file_name):
     return file_name
 
 
-def test_part1_code():
-    """
-    Run examples that test the functions for part 1
-    """
+# def test_part1_code():
+#     """
+#     Run examples that test the functions for part 1
+#     """
     
-    # Simple test for reader and writer
-    test_table = read_csv_file("test_case.csv")  # create a small CSV for this test
-    print(test_table)
-    print_table(test_table)
-    print()
-    write_csv_file(test_table, "table_written.csv")
+#     # Simple test for reader and writer
+#     test_table = read_csv_file("test_case.csv")  # create a small CSV for this test
+#     print(test_table)
+#     print_table(test_table)
+#     print()
+#     write_csv_file(test_table, "table_written.csv")
 
-    # Test the writer
-    cancer_risk_table = read_csv_file("cancer_risk05_v4_county.csv")
-    write_csv_file(cancer_risk_table, "cancer_risk05_v4_county_copy.csv")
-    cancer_risk_copy = read_csv_file("cancer_risk05_v4_county_copy.csv")
+#     # Test the writer
+#     cancer_risk_table = read_csv_file("cancer_risk05_v4_county.csv")
+#     write_csv_file(cancer_risk_table, "cancer_risk05_v4_county_copy.csv")
+#     cancer_risk_copy = read_csv_file("cancer_risk05_v4_county_copy.csv")
     
-    # Test whether two tables are the same
-    for row in range(len(cancer_risk_table)):
-        for col in range(len(cancer_risk_table[0])):
-            if cancer_risk_table[row][col] != cancer_risk_copy[row][col]:
-                print("Difference at"), row, col, cancer_risk_table[row][col], cancer_risk_copy[row][col]
+#     # Test whether two tables are the same
+#     for row in range(len(cancer_risk_table)):
+#         for col in range(len(cancer_risk_table[0])):
+#             if cancer_risk_table[row][col] != cancer_risk_copy[row][col]:
+#                 print("Difference at"), row, col, cancer_risk_table[row][col], cancer_risk_copy[row][col]
            
 
-test_part1_code()
+# test_part1_code()
+
+
+#########################################################
+# Part 2 - Week 4
+
+
+def select_columns(my_table, col_indices):
+    """
+    Input: Nested list my_table and a list of integers col_indices
+    Output: Nested list corresponding to sub-table formed by
+    columns in col_indices
+    """
+    nested_list = []
+    for row in my_table:
+        select_list = [row[col] for col in col_indices]
+        nested_list.append(select_list)
+    return nested_list
+
+
+def sort_by_column(my_table, col_idx):
+    """
+    Input: Nested list my_table and an integer col_idx
+    Action: Mutate the order of the rows in my_table such that the entries in
+    the column col_idx appear in DESCENDING order when interpreted as numbers
+    """
+    
+    pass
+    
+    
+    
+def test_part2_code():
+    """
+    Run examples that test the functions for part 2
+    """
+    
+    # Load a simple example table
+    test_table = read_csv_file("test_case.csv")  # file is available at ...
+    print_table(test_table)
+    print()
+    
+    # Simple test for column trimmng function
+    print_table(select_columns(test_table, [0, 2]))
+    print()
+    
+    # # Simple test for column sorting function
+    # sort_by_column(test_table, 3)
+    # print_table(test_table)
+    # print()
+
+    # # Read cancer-risk data set, select columns A, B, C, E, and L, then sort by column E in descending order
+    # cancer_risk_table = read_csv_file("cancer_risk05_v4_county.csv")
+    # col_indices = [0, 1, 2, 4, 11]
+    # trimmed_risk_table = select_columns(cancer_risk_table, col_indices)
+    # sort_by_column(trimmed_risk_table, 4)
+    # write_csv_file(trimmed_risk_table, "cancer_risk_trimmed.csv")
+    
+    # # Load our file "cancer_risk_trimmed_solution.csv" and compare with your solution
+    # trimmed_risk_solution = read_csv_file("cancer_risk_trimmed_solution.csv")
+    
+
+test_part2_code()
+
+#Output from test_part2_code()
+##['1', '2', '3', '4']
+##['5', '6', '7', '8']
+##['-2', '-3', '-4', '-5']
+##
+##['1', '3']
+##['5', '7']
+##['-2', '-4']
+##
+##['5', '6', '7', '8']
+##['1', '2', '3', '4']
+##['-2', '-3', '-4', '-5']
 
     
